678c061082950a7ac539fb3cf3af9a3f8566d11a
sammi-jiang28 <sammi.jiang28@gmail.com>
2025-09-13 05:01:10 -0400

    colours
    colours

--- src/app/components/overviewBox.tsx
+++ src/app/components/overviewBox.tsx
@@ -5,18 +5,19 @@ type OverviewBoxProps = {
     title: string;
     value: string | number;
     note: string;
+    noteColour?: string;
     icon?: ReactNode;
     iconColour?: string;
     iconBackgroundColour: string;
   };
 
-  export default function OverviewBox({ title, value, note, icon, iconColour, iconBackgroundColour }: OverviewBoxProps) {
+  export default function OverviewBox({ title, value, note, noteColour, icon, iconColour, iconBackgroundColour }: OverviewBoxProps) {
     return (
       <div className="p-4 rounded-2xl shadow bg-white grid grid-cols-2 w-76 mx-4">
         <div className='col-start-1 ml-4'>
         <h3 className="text-sm font-medium text-gray-500">{title}</h3>
         <p className="text-2xl font-bold text-gray-900">{value}</p>
-        <p className="text-xs ">{note}</p>
+        <p className={`text-xs ${noteColour}`}>{note}</p>
         </div>
         {icon && (
   <div   
